a5d5eac32bd8faf3149c93eb08fee247b1a2b0aa
Aubrey Neimeier <neimeier.1@osu.edu>
2018-04-25 17:24:26 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,8 @@ class PostsController < ApplicationController
 
 	def create
 		post = Post.new
-  post.title = params[:title]
-  post.description = params[:description]
-  post.save
-end
+	  post.title = params[:title]
+	  post.description = params[:description]
+	  post.save
+	end
 end
